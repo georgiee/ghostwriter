daf5656bd82f3a9880fbccecfaeaa85011356f01
Georgios Kaleadis <geokal@Georgioss-MacBook-Pro.local>
2017-07-12 00:23:05 +0200

    color and shortcode for examples updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 require("./styles/index.scss");
-console.log('hello2');
 
 class ExampleViewer {
-  constructor(){
-    console.log('hello1')
+  constructor(el){
+    this.el = el;
+    this.src = this.el.getAttribute('data-src');
+
+    this.iframe = this.el.querySelector('.js-iframe');
+    this.playButton = this.el.querySelector('.js-play');
+    this.playButton.addEventListener('click', event => {
+      this.play();
+    })
+  }
+
+  play(){
+    this.el.classList.add('is-playing');
+    this.iframe.setAttribute('src', this.src);
   }
 }
 
 
 window.addEventListener('load', function() {
-  new ExampleViewer();
+  const elements = document.querySelectorAll('.js-example-view');
+  for(let i = 0, l = elements.length; i < l; i++) {
+    new ExampleViewer(elements[i]);
+  }
+
 })
